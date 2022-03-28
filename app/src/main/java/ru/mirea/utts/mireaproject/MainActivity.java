8dea99cbebfe2969b40c0cb22bdba14c4d8a9d6f
uttsvlad <uttsvlad@gmail.com>
2022-03-28 19:09:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/utts/mireaproject/MainActivity.java
@@ -0,0 +1,100 @@
+package ru.mirea.utts.mireaproject;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mirea.utts.mireaproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_calculator, R.id.nav_web)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String vyr = editText.getText().toString();
+        String[] arr = vyr.split(" ");
+        TextView textView = findViewById(R.id.textView3);
+        double result = 0;
+
+        try {
+            double x = Double.parseDouble(arr[0]);
+            double y = Double.parseDouble(arr[2]);
+            switch (arr[1]) {
+                case "+":
+                    result = x + y;
+                    break;
+                case "-":
+                    result = x - y;
+                    break;
+                case "*":
+                    result = x * y;
+                    break;
+                case "/":
+                    result = x / y;
+                    break;
+            }
+
+            textView.setText(Double.toString(result));
+        } catch (Exception e) {
+            textView.setText("Вы ввели некорректное выражение");
+        }
+    }
+}
---
+++ app/src/main/java/ru/mirea/utts/mireaproject/fragments/WebFragment.java
@@ -0,0 +1,81 @@
+package ru.mirea.utts.mireaproject.fragments;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import ru.mirea.utts.mireaproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WebFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class WebFragment extends Fragment {
+    private WebView webView;
+    private EditText searchInput;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment web.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WebFragment newInstance(String param1, String param2) {
+        WebFragment fragment = new WebFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WebFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
+        view.findViewById(R.id.search_button).setOnClickListener(this::onSearchButtonClick);
+        searchInput = (EditText) view.findViewById(R.id.url_input);
+        webView = (WebView) view.findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com/?hl=ru");
+        return view;
+    }
+
+    private void onSearchButtonClick(View view){
+        webView.loadUrl(searchInput.getText().toString());
+    }
+}
